01d2b179caf56c03a7d8bac41fd7e8fb56825816
Adrien Canterot <adrien@nutsh.com>
2009-04-25 11:51:09 +0000

    suppresion de nutsh-05.pro

--- src/preprocess.h
+++ src/preprocess.h
@@ -5,7 +5,7 @@
 #define REQUETE(q) QSqlQuery requete; if(!requete.exec(q)) { qDebug() << requete.lastError() << " | Q = " << requete.lastQuery(); }
 #define QUERY(q) if(!query.exec(q)) { qDebug() << query.lastError() << " | \n\nQ = " << query.lastQuery() << "\n\n"; }
 #define NB_CHAMPS_DATABASE 13
-#define FORMATS_SUPPORTES "*.mp3" << "*.ogg" << "*.aac"
+#define FORMATS_SUPPORTES "*.mp3" << "*.ogg" << "*.aac" << "*.m4u";
 #define D(bug) qDebug() << bug;
 
 #endif // PREPROCESS_H
